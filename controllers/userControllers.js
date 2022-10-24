6fa0cb2f528985a232a1759594ed3d512b7be3cf
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-10-24 15:57:38 -0400

    Installed jsonwebtoken and bcryptjs for user auth and to hash user passwords

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,5 +1,7 @@
 const { PrismaClient } = require("@prisma/client");
 
+const { hash, compare } = require("bcryptjs");
+
 const prisma = new PrismaClient();
 
 // exports.getAllUsers = async (req, res) => {
@@ -8,19 +10,38 @@ const prisma = new PrismaClient();
 //       published: true,
 //     },
 //     include: { author: true },
+
 //   });
 //   res.json(allPosts);
 // };
 
+//register a user
 exports.createUser = async (req, res) => {
-  const { firstName, lastName, email } = req.body;
+  const { firstName, lastName, email, password } = req.body;
+  try {
+    //check if user exists
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+    });
 
-  const result = await prisma.user.create({
-    data: {
-      firstName,
-      lastName,
-      email,
-    },
-  });
-  res.json(result);
+    //throw error if user exists
+    if (user) throw new Error("User already exists.");
+
+    //if user does not exist, hash the password
+    const hashedPassword = await hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+      },
+    });
+    res.json(newUser);
+  } catch (err) {
+    res.send({
+      error: `${err.message}`,
+    });
+  }
 };
