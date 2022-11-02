bc76b8de28925a145e2f68be01f359ec9f9fac5e
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-11-02 17:36:11 -0400

    Updated schema for unique following/follower id and unique like/post relationship. Added cascade on delete constraint for users and posts

---
+++ prisma/migrations/20221102212348_edit_like_follow_tables/migration.sql
@@ -0,0 +1,57 @@
+/*
+  Warnings:
+
+  - The primary key for the `Follow` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `id` on the `Follow` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[userId,postId]` on the table `Like` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Comment" DROP CONSTRAINT "Comment_postId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Comment" DROP CONSTRAINT "Comment_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Follow" DROP CONSTRAINT "Follow_followerId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Follow" DROP CONSTRAINT "Follow_followingId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Like" DROP CONSTRAINT "Like_postId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Like" DROP CONSTRAINT "Like_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- AlterTable
+ALTER TABLE "Follow" DROP CONSTRAINT "Follow_pkey",
+DROP COLUMN "id",
+ADD CONSTRAINT "Follow_pkey" PRIMARY KEY ("followerId", "followingId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Like_userId_postId_key" ON "Like"("userId", "postId");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Follow" ADD CONSTRAINT "Follow_followerId_fkey" FOREIGN KEY ("followerId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Follow" ADD CONSTRAINT "Follow_followingId_fkey" FOREIGN KEY ("followingId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
