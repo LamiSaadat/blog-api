c1b863f69c7099b38ef0987eef12ff413f41a69e
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-11-04 12:29:01 -0400

    Dropped boolean follow column from Follow table since it is not needed in the relationship

---
+++ prisma/migrations/20221104142238_edit_follow_and_user_tables_1/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `follow` on the `Follow` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Follow" DROP COLUMN "follow";
